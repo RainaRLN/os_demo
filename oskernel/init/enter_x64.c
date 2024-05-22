64f4a97956d89f473357b910d413042240e85906
lina <raina.rao@outlook.com>
2024-05-22 14:52:20 +0800

    进入 IA-32e 模式

--- oskernel/init/enter_x64.c
+++ oskernel/init/enter_x64.c
@@ -1,5 +1,14 @@
 #include "enter_x64.h"
 #include "linux/kernel.h"
+#include "asm/system.h"
+#include "string.h"
+
+// Vol.3A 4-23
+// 所有表的地址都是 4K 对齐的
+#define PML4T_ADDR 0x8000
+#define PDPT_ADDR 0x9000
+#define PD_ADDR 0xA000
+// #define PT_ADDR 0xB000
 
 // Vol.2A 3-198
 // 如果可以改变 EFLAGS 的 bit21，表示支持 CPUID 指令
@@ -151,5 +160,101 @@ int x64_cpu_check(void)
 
     get_mem_info();
     return 1;
+}
+
+static void load_pml4(void)
+{
+    // Vol.3A 4-2: CR4.PAE=1, IA32_EFER.LME=1, CR4.LA57=0, 4-level paging is uesd.
+    __asm__ __volatile__(
+        "mov eax, cr4 \n\t"
+        "btr eax, 12 \n\t"
+        "mov cr4, eax \n\t"
+    );
+
+    // Page map level 4 table
+    int *plm4e_ptr = (int *)PML4T_ADDR;
+
+    // Vol.3A 4-6: Every paging structure is 4096 Bytes in size
+    memset(plm4e_ptr, 0, 4096);
+
+    // Vol.3A 4-25 Table 4-15
+    *plm4e_ptr = PDPT_ADDR | 3;  // R/W | Present
+    *(plm4e_ptr + 1) = 0;
+
+    // Page directory pointer table
+    int *pdpte_ptr = (int *)PDPT_ADDR;
+    memset(pdpte_ptr, 0, 4096);
+
+    // Vol.3A 4-27 Table 4-17
+    *pdpte_ptr = PD_ADDR | 3;  // R/W | Present
+    *(pdpte_ptr + 1) = 0;
+
+    // Page directory
+    int *pde_ptr = (int *)PD_ADDR;
+    memset(pde_ptr, 0, 4096);
+
+    // 采用 2M 分页，直接映射 0 - 100M
+    for (int i = 0; i < 50; ++i) {
+        // Vol.3A 4-27 Table 4-18
+        *(pde_ptr + i * 2) = 0x200000 * i | 0x83;  // Page size | R/W | Present
+        *(pde_ptr + i * 2 + 1) = 0;
+    }
+
+    // Vol.3A 4-20 Table 4-12
+    __asm__ __volatile__(
+            "mov cr3, ebx \n\t"
+            :
+            :"b"(PML4T_ADDR)
+            :
+    );
+}
+
+// Vol.3A 9-11 9.8.5
+static void init_ia32e_mode(void)
+{
+    // 1. 禁用分页模式: CR0.PG = 0
+    //    Vol.3A 2-14 Figure 2-7. Control Registers
+    __asm__ __volatile__(
+        "mov eax, cr0 \n\t"
+        "btr eax, 31 \n\t"
+        "mov cr0, eax \n\t"
+    );
+
+    // 2. 开启物理地址扩展功能: CR4.PAE = 1
+    __asm__ __volatile__(
+        "mov eax, cr4 \n\t"
+        "bts eax, 5 \n\t"
+        "mov cr4, eax \n\t"
+    );
+
+    // 3. 加载四级页表地址到 CR3
+    //    此时在保护模式，只有 CR3 的低 32 位被写入
+    //    所以 64 位模式的页表必须位于内存的低 4G 内
+    //    在激活 IA-32e 模式后，可将页表重新定位到物理内存的任意位置
+    load_pml4();
+
+    // 4. 使能 IA-32e 模式: IA32_EFER.LME = 1
+    //    Vol.4 2-50: IA32_EFER 的 MSR 地址为 0xC0000080
+    //    Vol.2B 4-534: RDMSR Description
+    __asm__ __volatile__(
+        "mov ecx, 0xc0000080 \n\t"
+        "rdmsr \n\t"
+        "bts eax, 8 \n\t"
+        "wrmsr \n\t"
+    );
+
+    // 5. 开启分页: CR0.PG = 1
+    __asm__ __volatile__(
+        "mov eax, cr0 \n\t"
+        "bts eax, 31 \n\t"
+        "mov cr0, eax \n\t"
+    );
+}
+
+void enter_x64(void)
+{
+    BOCHS_DEBUG();
+    init_ia32e_mode();
+    BOCHS_DEBUG();
 }
 
